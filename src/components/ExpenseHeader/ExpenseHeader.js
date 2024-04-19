95d91bc54da6d2f0e21b7a2419ca822ab1249099
Viral Desai <viral9597@gmail.com>
2024-04-19 11:15:04 -0700

    CSS changes and validations

--- src/components/ExpenseHeader/ExpenseHeader.js
+++ src/components/ExpenseHeader/ExpenseHeader.js
@@ -1,13 +1,14 @@
 import React from 'react';
-import logo from '../../assets/expense-tracker-big-logo.jpg';
+import logo from '../../assets/expense-tracker-logo.jpg';
 import './ExpenseHeader.css';
 
 const ExpenseHeader = () => {
 
     return (
         <header className="header">
+           <img src={logo} alt="logo" />
           <h1>Expense Tracker</h1>
-        <img src={logo} alt="logo" />        
+               
       </header>
     );    
 };
--- src/components/NewExpense/ExpenseForm.js
+++ src/components/NewExpense/ExpenseForm.js
@@ -39,15 +39,15 @@ const ExpenseForm = (props) => {
         <div className="new-expense__controls">
             <div className="new-expense__control">
                 <label>Title</label>
-                <input type="text"  value={enteredTitle} onChange={titleChangeHandler}></input>
+                <input type="text"  value={enteredTitle} onChange={titleChangeHandler} required></input>
             </div>
             <div className="new-expense__control">
                 <label>Amount</label>
-                <input type="number"  value={enteredAmount} min="0.01" step="0.01" onChange={amountChangeHandler} ></input>
+                <input type="number"  value={enteredAmount} min="0.01" step="0.01" onChange={amountChangeHandler} required></input>
             </div>
             <div className="new-expense__control">
                 <label>Date</label>
-                <input type="date" value={enteredDate} min="2019-01-01" max="2022-12-31" onChange={dateChangeHandler}></input>
+                <input type="date" value={enteredDate} min="2019-01-01" max="2024-12-31" onChange={dateChangeHandler} required></input>
             </div>
         </div>
         <div className="new-expense__actions">
